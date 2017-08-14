9bff41d02d4fe0deb3765d304e9785500b41bbab
harpreet97 <harrycheema97@yahoo.com>
2017-08-14 12:02:02 +1200

    initila commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task2_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plese type a number");
+            string input = Console.ReadLine();
+            int output = 0;
+
+            if (int.TryParse(input, out output))
+            {
+                Console.WriteLine("Entered number is:" + output);
+                if (output > 10)
+                {
+
+                }
+                else
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        int total=output/i;
+                        Console.WriteLine(string.Format("{0}/{1}={2}",output,i,total));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
